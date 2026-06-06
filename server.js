6ebe81e9548494c1fa9acf2c903e5164256484f7
A-bhi123 <abhishekkumarbind616@gmail.com>
2026-06-06 08:52:15 +0530

    update the server.js

--- server.js
+++ server.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: process.env.CLIENT_URL || 'http://localhost:5173',
+    origin: ['https://watch-together-frontend-lilac.vercel.app', 'http://localhost:5173'],
     methods: ['GET', 'POST'],
     credentials: true,
   },
